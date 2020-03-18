ac40d42d19844641ccf6854a6fd5c6a4097f2133
Junyuan <junyuantanhere@gmail.com>
2020-03-19 03:55:22 +0800

    Modify Course4 Lec3 Assignment

--- src/src/course4/TspHeuristic.java
+++ src/src/course4/TspHeuristic.java
@@ -8,18 +8,21 @@ import java.util.ArrayList;
 
 public class TspHeuristic {
     static int nodeNum;
-
+    static double totalDis=0;
     public static void main(String[] args) throws IOException {
         ArrayList<tspNode> tspNodes = readTxt("C:\\Users\\Junyuan Tan\\Desktop\\stanford_alg\\nn.txt");
         ArrayList<tspNode> routes =new ArrayList<>();
         ArrayList<tspNode> remainNodes= (ArrayList<tspNode>)tspNodes.clone();
-        double totalDis=0;
+        ArrayList<Double> distance =new ArrayList<Double>();
         routes.add(remainNodes.get(0));
         remainNodes.remove(0);
         while (remainNodes.size()>0){
-            tspNode addNode=findNearest(remainNodes, routes.get(routes.size()-1),totalDis);
+            tspNode addNode=findNearest(remainNodes, routes.get(routes.size()-1),distance);
             routes.add(addNode);
             remainNodes.remove(addNode);
+        }
+        for(double dis:distance){
+            totalDis+=dis;
         }
         totalDis+=calDistance(routes.get(0), routes.get(routes.size()-1));
         System.out.println("启发式方法计算得到的道路长度为： "+totalDis);
@@ -34,24 +37,25 @@ public class TspHeuristic {
         int id = 0;
         while ((line = bufferedReader.readLine()) != null) {
             id++;
-            tspNodes.add(new tspNode(id, Double.parseDouble(line.split(" ")[0]), Double.parseDouble(line.split(" ")[1])));
+            tspNodes.add(new tspNode(id, Double.parseDouble(line.split(" ")[1]), Double.parseDouble(line.split(" ")[2])));
         }
         return tspNodes;
     }
 
-    private static tspNode findNearest(ArrayList<tspNode> remainNodes, tspNode tail, double totalDis) {
+    private static tspNode findNearest(ArrayList<tspNode> remainNodes, tspNode tail,ArrayList<Double>distance) {
         double shortestDis = Integer.MAX_VALUE;
         tspNode nearestNode = null;
         for (tspNode node : remainNodes) {
             if (shortestDis > calDistance(node, tail)) nearestNode = node;
             shortestDis = shortestDis < calDistance(node, tail) ? shortestDis : calDistance(node, tail);
         }
-        totalDis+=shortestDis;
+        System.out.println(shortestDis);
+        distance.add(shortestDis);
         return nearestNode;
     }
 
     private static double calDistance(tspNode node1, tspNode node2) {
-        return Math.sqrt(Math.pow(node1.x - node2.x, 2) + Math.pow(node2.y - node2.y, 2));
+        return Math.sqrt(Math.pow(node1.x - node2.x, 2) + Math.pow(node1.y - node2.y, 2));
     }
 
     private static class tspNode {
